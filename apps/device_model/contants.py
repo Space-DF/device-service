5ef65271ed4efe7c023974dab996ee678ff9d38e
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2025-12-13 21:40:43 +0700

    fix: udpate device feature type

--- apps/device_model/contants.py
+++ apps/device_model/contants.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class KeyFeature(models.TextChoices):
-    WATER_LEVEL_SENSOR = "water_level_sensor"
-    MULTI_SENSOR_TRACKER = "multi_sensor_tracker"
+    WATER_DEPTH_TYPE = "water_depth"
+    LOCATION_TYPE = "location"
