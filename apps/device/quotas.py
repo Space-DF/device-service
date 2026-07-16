660d2b15d7b36817ad13ca1f3cb6b9bdb2820ce5
lethanhdat762003 <dat.le@df.technology>
2026-07-16 09:50:15 +0700

    fix: revert to use release actions

--- apps/device/quotas.py
+++ apps/device/quotas.py
@@ -2,7 +2,8 @@ from common.apps.billing.mixins import BaseQuota
 
 
 class DeviceQuota(BaseQuota):
-    reserve_actions = {"create", "bulk_create", "destroy"}
+    reserve_actions = {"create", "bulk_create"}
+    release_actions = {"destroy"}
     rules = {
         "create": "device.max_count",
         "bulk_create": "device.max_count",
